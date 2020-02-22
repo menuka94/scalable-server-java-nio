978e5f3084b9b7bb5b00aa417abb33f0807da62d
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-02-22 16:23:53 -0700

    chore: create project structure

---
+++ src/main/java/cs455/scaling/client/Client.java
@@ -0,0 +1,12 @@
+package cs455.scaling.client;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Client {
+    private static final Logger LOGGER = LogManager.getLogger(Client.class);
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cs455/scaling/server/Server.java
@@ -0,0 +1,12 @@
+package cs455.scaling.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Server {
+    private static final Logger LOGGER = LogManager.getLogger(Server.class);
+
+    public static void main(String[] args) {
+
+    }
+}
